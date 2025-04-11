aa2494434fc0a0dfe167e758bcfc1d27cc93a6fd
apovisr <angupr95@gmail.com>
2025-04-11 14:14:31 -0500

    feat: second version

--- business.rules.pl
+++ business.rules.pl
@@ -27,7 +27,7 @@ add_user_to_group(Person, CurrentMembers, [Person|CurrentMembers]).
 % add_user_to_group(juan, [], X).
 
 %=====================================================================================
-% caso de uso: Registrar gastos
+% use case: Registrar gastos
 
 % expenses of a group
 person_owes(mary, john, 50). % mary owes 50 to john
@@ -62,28 +62,42 @@ calculate_total_balance(Person) :-
 %=====================================================================================
 % Caso de uso; sugerir pagos
 
-total_balance(john, -100).
-total_balance(mary, 50).
-total_balance(paul, 50).
+total_balance(john, -30).
+total_balance(mary, -10).
+total_balance(paul, -10).
 total_balance(alex, -50).
+total_balance(jane, 100).
 
 
 suggest_payment(Person):-
     total_balance(Person, Total),
-    Total < 0 -> find_positive(-1 * Total), format('to ~w.~n', [Person]).
+    wrapperIf(Person, Total).
+
+wrapperIf(Person, Total):-
+    Total < 0 -> find_positive(-1 * Total), format('to ~w.~n', [Person]);
+    Total > 0 -> format('~w should pay.~n', [Person]), find_negative(-1 * Total).
+
 
 find_positive(Total):-
     findall(Person-Amount, (total_balance(Person, Amount), Amount >= 0), Debts),
     show_payments(Total, Debts).
 
 find_negative(Total):-
-        findall(Person-Amount, (total_balance(Person, Amount), Amount < 0), Debts),
-        show_payments(Total, Debts).
+    findall(Person-Amount, (total_balance(Person, Amount), Amount < 0), Debts),
+    show_payments_negative(Total, Debts).
+
 
 show_payments(Total, [Person-Amount| T]):-
     Total > Amount ->  format('~w should pay ~d.~n', [Person, Amount]), show_payments(Total - Amount, T), true;
     Total < Amount -> format('~w should pay ~d.~n', [Person, Total]);
     Total =:= Amount -> format('~w should pay ~d.~n', [Person, Total]).
 
+
+show_payments_negative(Total, [Person-Amount| T]):-
+    -1 * Total > -1 * Amount ->  format('~d to ~w.~n', [-1 *Amount, Person]), show_payments_negative(Total - Amount, T), true;
+    -1 * Total < -1 * Amount -> format('~d to ~w.~n', [-1 *Total, Person]);
+    -1 * Total =:= -1 * Amount -> format('~d to ~w.~n', [-1 *Total, Person]).
+
 % example
 % suggest_payment(john).
+% suggest_payment(jane).
